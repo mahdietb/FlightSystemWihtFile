679b50d3bc8ecf581f9228d72424cb1e661a0379
Mahdieh <ToRabimahdie1881@yahoo.com>
2023-05-25 17:15:49 +0430

    commit-4:add 4 other classes for files

---
+++ src/FlightFile.java
@@ -0,0 +1,2 @@
+public class FlightFile {
+}
--- src/Ticket.java
+++ src/Ticket.java
@@ -1,4 +1,25 @@
 public class Ticket {
     private String ticketId;
     private User user;
+
+
+    public Ticket(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
